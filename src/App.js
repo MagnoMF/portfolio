76a2c31925b9e700955873a20b713971c1c59be0
MagnoMF <magno_hmt@hotmail.com>
2022-10-01 13:13:56 -0300

    Style "sobre" criado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Sobre from "./components/Sobre";
 
 function App() {
   return (
     <React.Fragment>
       <Navbar/>
+      <Sobre/>
     </React.Fragment>
   );
 }
