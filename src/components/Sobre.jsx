431b7a731f159e0f406c7e5fa483209be5186b89
MagnoMF <magno_hmt@hotmail.com>
2022-11-20 12:55:08 -0300

    Separa estilização em um arquivo CSS e implementa a versão mobile.

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -1,48 +1,16 @@
 import React from "react";
 import fotoPerfil from "../img/foto-perfil.svg";
+import "../style/sobre.css";
 
 export default function Sobre() {
   return (
     <React.Fragment>
-      <div style={{ display: "flex", margin: "7rem 3em" }}>
-        <div
-          style={{
-            width: "0",
-            margin: "0",
-            //desktop
-            // width: "45%",
-            // display: "inline-block",
-            // margin: "auto",
-          }}
-        >
-          <img
-            style={{
-              width: "0",
-              margin: "0",
-              //desktop
-              // display: "flex",
-              // height: "10rem",
-              // margin: "auto",
-            }}
-            src={fotoPerfil}
-            alt="foto-perfil"
-          />
+      <div className="wrapp">
+        <div className="wrapp-image">
+          <img className="avatar-image" src={fotoPerfil} alt="foto-perfil" />
         </div>
-        <div
-          style={{
-            display: "inline-block",
-            margin: "auto",
-          }}
-        >
-          <p
-            style={{
-              fontSize: "20px",
-              display: "flex",
-              color: "white",
-              // textAlign: "right",
-              textAlign: "center",
-            }}
-          >
+        <div className="wrapp-text">
+          <p className="text">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit est
             harum suscipit hic. Eaque voluptatum, aliquid molestiae iure tempore
             sed consectetur repudiandae alias commodi at odit asperiores
